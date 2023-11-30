364d7c10a2928776756dd064fe73c4fd752cb231
leormston <l.e.ormston@gmail.com>
2023-11-30 11:02:14 +0000

    added recursive path traversal for python

---
+++ random/recursive_path_finding/python/recursivePathFinding.py
@@ -0,0 +1,61 @@
+#This represents up, down left and right. Saves us having to enter co-ords when walking
+directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1]
+]
+
+
+#maze is the maze string 2d array
+#wall - define what character represents a wall
+#curr is the current co-ords
+#end is if we are at the end
+#seen is a 2d array originally populated with falses but as we traverse gets populated with truthes
+#path is the path we are currently on
+def walk(maze, wall, curr, end, seen, path):
+
+    #Base cases
+    #1. Off the map
+    if(curr['x'] < 0 or curr['x'] >= len(maze[0]) 
+       or curr['y'] < 0 or curr['y'] >= len(maze)):
+        return False
+
+    #2. On a wall
+    if(maze[curr['y']][curr['x']] == wall):
+        return False
+    
+    #4. At the end
+    if(curr['x'] == end['x'] and curr['y'] == end['y']):
+        path.append(end)
+        return path
+    
+    #3. Already seen this block
+    if(seen[curr['y']][curr['x']]):
+        return False
+    
+    seen[curr['y']][curr['x']] = True
+    
+    path.append(curr)
+    
+    #recursive step
+    for direction in directions:
+        x, y =  direction[1], direction[0]
+        if walk(maze, 
+             wall, 
+             {'x': curr['x'] + x, 'y': curr['y'] + y},
+             end, seen, path):
+            return path
+
+if __name__ == "__main__":
+    print("Traversal Starting")
+    maze = [
+        ["#","#","#", "#", " ", "#", "#"],
+        ["#"," "," ", " ", " ", "#", "#"],
+        ["#"," ","#", "#", "#", "#", "#"]
+    ]
+    seen = [[False for x in range(len(maze[0]))] for y in range(len(maze))]
+    path = []
+
+    path = walk(maze, '#', {'x': 4, 'y': 0 }, {'x': 1, 'y': 2}, seen, path)
+    print(path)
